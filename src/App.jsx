c3ad6360905216445ee136ef4847729e85537bc6
AndyKristiania03 <nausherwan@Nausherwans-MacBook-Pro.local>
2025-04-01 22:54:58 +0500

    Project Setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+// src/App.tsx
+import React from 'react';
+
+const App = () => {
+  return (
+    <div style={{display: 'flex', justifyContent:'center', alignItems:'center'}}>
+      <h1>KWS2100 - Assignement 1</h1>
+    </div>
+  );
+};
+
+export default App;
