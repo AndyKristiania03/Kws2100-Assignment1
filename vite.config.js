35c8247e8c4eedda5c1a9cbc2ccbbe187357d2e1
AndyKristiania03 <nausherwan@Nausherwans-MacBook-Pro.local>
2025-04-02 00:45:07 +0500

    Deployment workflow fixes

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  basse: "/kws2100-assignment1"
+  basse: "/Kws2100-Assignment1"
 })
